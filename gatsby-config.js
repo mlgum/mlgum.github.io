b9d12e8533867c35744c31735002132eba4a0792
Ann Naser Nabil <ann.n.nabil@gmail.com>
2025-05-28 14:20:59 +0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/logo.png',
+        icon: 'favicon-32x32.png',
       },
     },
     `gatsby-plugin-offline`,
